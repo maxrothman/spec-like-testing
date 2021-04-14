060e392b26a99dba2320ac1d9fa8b9419f3f36e0
Max Rothman <max.r.rothman@gmail.com>
2021-04-13 22:11:06 -0400

    Refactoring around-each

--- src/libertest/core.clj
+++ src/libertest/core.clj
@@ -20,10 +20,10 @@
   [x]
   [::assertion (::name x) (::code x)])
 
-(defmacro group [name & forms]
-  `{::type ::group
-    ::name ~name
-    ::children ~(vec forms)})
+(defn group [name & trees]
+  {::type ::group
+   ::name name
+   ::children (vec trees)})
 
 (defmethod show ::group
   [x]
@@ -48,35 +48,62 @@
    (prn msg)
    x))
 
+(defn empty-node
+  "An placeholder node that does nothing except have children.
+   Used to work around splicing issues"
+  [children]
+  {::type ::empty
+   ::children children})
+
+(defmethod show ::empty
+  [x]
+  [::empty (mapv show (::children x))])
+
 ;; From https://groups.google.com/g/clojure/c/FIJ5Pe-3PFM
 ;; TODO: don't manually set :end, that's an impl detail
-(defn next-over [loc]
+;; Not needed currently, delete if not used by the time this is finished
+#_(defn next-over [loc]
   (cond
     (zip/end? loc) loc
     (some? (zip/right loc)) (zip/right loc)
     (some? (zip/up loc)) (recur (zip/up loc))
     :else (assoc loc 1 :end)))
 
-(defn around-each [f tree]
+(defn map-tree [filter-f map-f tree]
   (loop [loc (make-zipper tree)]
     (cond
-      (zip/end? loc) (zip/root loc)
-      (zip/branch? loc) (recur (zip/next loc))
-      :else (recur
-             (next-over
-              (zip/edit loc update ::code #(fn [] (f %))))))))
+      (zip/end?) (zip/root loc)
+      (filter-f loc) (recur (zip/next (map-f loc)))
+      :else (recur (zip/next loc)))))
 
-(defmethod show ::before
+(defn around-each [f & trees]
+  (empty-node
+   (vec
+    (for [tree trees]
+      (loop [loc (make-zipper tree)]
+        (cond
+          (zip/end? loc) (zip/root loc)
+          (zip/branch? loc) (recur (zip/next loc))
+          :else (recur
+                 (zip/next
+                  (zip/edit loc update ::code #(fn [] (f %)))))))))))
+
+(defn around [f tree]
+  nil)
+
+(defmethod show ::hook
   [x]
-  [::before (::code x) (mapv show (::children x))])
+  [::hook (::code x) (mapv show (::children x))])
 
 (comment
   (def tree (show (around-each (fn [f] (prn "outer") (f))
-                               (around-each (fn [f] (prn "inner") (f))
-                                            (group "foo"
-                                                   (assertion "a1" (test/is (= 1 (inc 0))))
-                                                   (group "bar"
-                                                          (assertion "a2" (test/is (not= 2 (inc 3))))))))))
+                               (group "baz"
+                                      (around-each (fn [f] (prn "inner") (f))
+                                                   (group "foo"
+                                                          (assertion "a1" (test/is (= 1 (inc 0))))
+                                                          (group "bar"
+                                                                 (assertion "a2" (test/is (not= 2 (inc 3))))))
+                                                   (assertion "a3" (test/is (= 1 (dec 2)))))))))
   (assertion (test/is (= 1 1)))
 
   (map (comp :name zip/node)
