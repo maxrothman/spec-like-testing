d76128c3869ecdec051bbda524382e69bf74a6b3
Max Rothman <max.r.rothman@gmail.com>
2021-04-13 20:41:27 -0400

    Basic each hook: around-each working

--- src/libertest/core.clj
+++ src/libertest/core.clj
@@ -34,6 +34,7 @@
               (comp seq ::children)
               (fn [node children] (assoc node ::children children)
                 ;; I don't really understand why this is wrong?
+                ;; I guess "make-node" would be better named as "set-children"?
                 #_(update node ::children #(into (or % []) children)))
               root))
 (def node (assertion "foo" inc))
@@ -56,39 +57,26 @@
     (some? (zip/up loc)) (recur (zip/up loc))
     :else (assoc loc 1 :end)))
 
-(defn before-each* [f tree]
+(defn around-each [f tree]
   (loop [loc (make-zipper tree)]
     (cond
       (zip/end? loc) (zip/root loc)
       (zip/branch? loc) (recur (zip/next loc))
       :else (recur
              (next-over
-              (zip/replace loc
-                           (zip/make-node loc
-                                          {::type ::before
-                                           ::code f}
-                                          [(zip/node loc)])))))))
-
-(defmacro before-each [form & forms]
-  `(before-each* ~form #_(fn [] ~form) ~@forms))
-;; TODO: doing this so I can tell befores apart while debugging
+              (zip/edit loc update ::code #(fn [] (f %))))))))
 
 (defmethod show ::before
   [x]
   [::before (::code x) (mapv show (::children x))])
 
-;; Problem: before-eaches should get wrapped on in the order they appear in the tree. Since the
-;; outer before-each happens after the inner, it currently goes in deeper.
-;; Maybe this is a good opportunity for clojure's hierarchies? To test if a node is one of the ones
-;; to stop looking for things to wrap at
-;; Will need to move the current else clause backwards 1, and maybe don't need an else at all?
 (comment
-  (def tree (show (before-each "yo"
-                   (before-each "hi"
-                                (group "foo"
-                                       (assertion "a1" (test/is (= 1 (inc 0))))
-                                       (group "bar"
-                                              (assertion "a2" (test/is (not= 2 (inc 3))))))))))
+  (def tree (show (around-each (fn [f] (prn "outer") (f))
+                               (around-each (fn [f] (prn "inner") (f))
+                                            (group "foo"
+                                                   (assertion "a1" (test/is (= 1 (inc 0))))
+                                                   (group "bar"
+                                                          (assertion "a2" (test/is (not= 2 (inc 3))))))))))
   (assertion (test/is (= 1 1)))
 
   (map (comp :name zip/node)
